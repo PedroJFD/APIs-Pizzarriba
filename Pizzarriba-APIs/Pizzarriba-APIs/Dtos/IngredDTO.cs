61aac9e45542b3485ee2c0747a088225a1210c6f
CassianeAlmeida <cassianealmeida66@gmail.com>
2024-09-26 16:22:58 -0400

    Ingrediente Commit

---
+++ Pizzarriba-APIs/Pizzarriba-APIs/Dtos/IngredDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IngredienteAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IngredienteAPI.Dtos
+{
+    public class IngredDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Fornecedor { get; set; }
+        [Required]
+        public string Medida { get; set; }
+        [Required]
+        public double Quantidade { get; set; }
+
+    }
+}
---
+++ Pizzarriba-APIs/Pizzarriba-APIs/Models/Ingred.cs
@@ -0,0 +1,12 @@
+﻿namespace IngredienteAPI.Models
+{
+    public class Ingred
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Fornecedor { get; set; }
+        public string Medida { get; set; }
+        public double Quantidade { get; set; }
+
+    }
+}
